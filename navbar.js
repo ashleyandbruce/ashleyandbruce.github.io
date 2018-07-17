5d105bc8e8a0f29baa14ec7c675a660318fea02e
DESKTOP-F1VAQ0V\ashle <ashley.currie@ucalgary.ca>
2018-07-17 11:34:56 -0600

    Put title at top of page

---
+++ navbar.js
@@ -0,0 +1,18 @@
+
+window.onscroll = function(){
+	scroll();
+};
+
+var navbar =  document.getElementById("topnav"); 
+var	sticky = navbar.offsetTop;
+
+
+function scroll(){
+	
+	if(window.pageYOffset >= sticky){
+		navbar.classList.add("sticky");
+	}
+	else{
+		navbar.classList.remove("sticky");
+	}
+}
