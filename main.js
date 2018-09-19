2f1fca3c20fc6647130cd9464f327def77db0e39
ashley_currie <ashleycurr@live.ca>
2018-09-18 22:08:26 -0600

    Added introductory details

--- main.js
+++ main.js
@@ -1,36 +1,18 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
+	
+	var offset = 150;
+	var duration = 300;
+	
+	$(window).scroll(function(){
+		if($(this).scrollTop() > offset){
+			$('.up').fadeIn(duration);
+		}
+		else{
+			$('.up').fadeOut(duration);
+		}
+	});
 
-	var index = 0;
-	var images =["res/index.jpg", "res/details.jpg", "res/RSVP.jpg"];
-		
-	slideshow();
-	
-	function slideshow(){
-		
-		index= (index + 1)% 3;
-		
-		$('.header-img').css('background-image', 'url("'+ images[index] + '")')
-		 .fadeIn("slow" , function(){
-			setTimeout(slideshow,4000);
-		});
-	}
-	
 });
-
-
-function scroll_down(){
-	
-	var top_section = $('.top-section').offset();
-	
-	$('html, body').animate({
-		
-		scrollTop: top_section.top
-	
-	}, 1000);
-
-
-	return false;
-}
 
 function scroll_up(){
 	
