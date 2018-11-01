d0b82802806334e18e1395f4fd8e9b827218cc9f
ashley_currie <ashleycurr@live.ca>
2018-11-01 01:48:49 -0600

    Added get call for guest's party

--- rsvp.js
+++ rsvp.js
@@ -4,17 +4,31 @@ $(document).ready(function(){
 	$('#submit').click(function(){
 		var first_name = $('.input-first-name').val();
 		var last_name = $('.input-last-name').val();
-		get_guest(first_name, last_name);
+		var guest = get_guest(first_name, last_name);
+		var party = get_party(guest.party_id);
 	});
 
 });
 
 
 function get_guest(first_name, last_name){
+	var guest;
 	$.get(
 		`https://quiet-plains-44094.herokuapp.com/guests/${first_name}/${last_name}`,
 		function(data){
-			console.log(data);
+			guest = data;
 		}
 	);
+	return guest;
+}
+
+function get_party(party_id){
+	var party;
+	$.get(
+		`https://quiet-plains-44094.herokuapp.com/guests/${party_id}`,
+		function(data){
+			party = data;
+		}
+	);
+	return party;
 }
