331ae01371d1b7ff82182ba2d45cb0db54a69006
ashley_currie <ashleycurr@live.ca>
2018-10-31 23:55:46 -0600

    Changed rsvp form

--- rsvp.js
+++ rsvp.js
@@ -1,33 +1,20 @@
 $(document).ready(function(){
-
-	$('#add-person').click(function(){
-		var add_person_form = $('#rsvp-form-temp').html();
-		$(add_person_form).insertAfter('.rsvp-form:last');
+	
 		
-		$('.remove-person').click(function(){
-			$(this).parents('.rsvp-form').remove();
-		});		
-	});
-	
-	
 	$('#submit').click(function(){
-		var message = [];
-		var rsvps = $('.rsvp-form');
-		
-		for (var rsvp of rsvps){
-			message.push([
-				$(rsvp).find('.input-first-name').val(),
-				$(rsvp).find('.input-last-name').val(),
-				$(rsvp).find('.select-rsvp-status').val()
-			]);
-		}
-		
-		
+		var first_name = $('.input-first-name').val();
+		var last_name = $('.input-last-name').val();
+		get_guest(first_name, last_name);
 	});
 
 });
 
 
-function google_sheets(data){
-
+function get_guest(first_name, last_name){
+	$.get(
+		`https://quiet-plains-44094.herokuapp.com/guests/${first_name}/${last_name}`,
+		function(data){
+			console.log(data);
+		}
+	);
 }
