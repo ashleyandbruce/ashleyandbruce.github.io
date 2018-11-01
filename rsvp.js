26c2973b17e4d61ffb08ab453ba6492755aa54d2
ashley_currie <ashleycurr@live.ca>
2018-11-01 09:15:20 -0600

    Added error callback and error text to page

--- rsvp.js
+++ rsvp.js
@@ -1,37 +1,35 @@
 $(document).ready(function(){
 	
-		
 	$('#submit').click(function(){
 		var first_name = $('.input-first-name').val();
 		var last_name = $('.input-last-name').val();
 		var guest = get_guest(first_name, last_name);
-		console.log(guest);
 		var party = get_party(guest.party_id);
-		console.log(party);
 	});
 
 });
 
 
 function get_guest(first_name, last_name){
-	var guest;
+
 	$.get(
 		`https://quiet-plains-44094.herokuapp.com/guests/${first_name}/${last_name}`,
 		function(data){
-			guest = data;
 			console.log(data);
 		}
-	);
-	return guest;
+	).fail(function(){
+	
+	
+	});
 }
 
 function get_party(party_id){
-	var party;
+
 	$.get(
 		`https://quiet-plains-44094.herokuapp.com/guests/${party_id}`,
 		function(data){
 			party = data;
 		}
 	);
-	return party;
+
 }
