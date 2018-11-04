823571f5b151854d477573e344d941d8de096c64
ashley_currie <ashleycurr@live.ca>
2018-11-03 20:54:00 -0600

    Logging primary guest information

--- rsvp.js
+++ rsvp.js
@@ -7,6 +7,9 @@ function MainVM(){
 	
 	this.submit = async function(){
 		var guest = await client.getGuest(this.firstName(), this.lastName());
+		
+		console.log(guest);
+		
 		var party = await client.getParty(guest.party_id);
 		
 		console.log(party);
@@ -81,77 +84,3 @@ function GuestClient(){
 $(document).ready(function(){
 	ko.applyBindings(new MainVM());
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$(document).ready(function(){
-	
-	$('#submit').click(function(){
-		var first_name = $('.input-first-name').val();
-		var last_name = $('.input-last-name').val();
-		get_guest(first_name, last_name);
-	});
-
-});
-
-function on_error(){
-	$('#error').removeClass('invisible').addClass('visible');
-}
-
-function on_success(){
-	$('#error').removeClass('visible').addClass('invisible');
-}
-
-function clear_form_wrapper(){
-	$('#dynamic-form-wrapper').empty();
-}
-
-function get_guest(first_name, last_name){
-
-	$.get(
-		`https://quiet-plains-44094.herokuapp.com/guests/${first_name}/${last_name}`,
-		function(data){
-			if(data.length == 0){
-				on_error();
-			}
-			else{
-				var guest = data[0];
-				clear_form_wrapper();
-				get_party(guest.party_id);
-			}
-		}
-	);
-}
-
-function get_party(party_id){
-
-	$.get(
-		`https://quiet-plains-44094.herokuapp.com/guests/${party_id}`,
-		function(guests){
-			for(guest of guests){
-				var temp = $('#rsvp-guest-form').html();
-				$('#dynamic-form-wrapper').append(temp);
-				$(temp).find('.guest-name').append(`${guest.first_name} ${guest.last_name}`);
-			}
-
-		}
-	);
-
-}*/
