186385fa319e2b67a7bc8b27e81f4bf2d00d5eef
ashley_currie <ashleycurr@live.ca>
2019-02-19 23:13:44 -0800

    Deployment on main branch

--- src/app/interfaces/app-state.interface.ts
+++
@@ -1,8 +0,0 @@
-import { GetGuestsState } from '../reducers/get-guests.reducer';
-import { AddGuestsState } from '../reducers/add-guests.reducer';
-
-export interface IAppState{
-	loading : boolean;
-	getGuests : GetGuestsState;
-	addGuests : AddGuestsState;
-}
